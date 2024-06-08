4de7600e2807253d82ad9795c9fd200093cc8e35
Édipo Almeida <almeida.edipo@icloud.com>
2024-06-08 21:00:29 +0100

    15. Testing first domain class

--- cursomc/src/main/java/com/ealmeida/springbootionic/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/ealmeida/springbootionic/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,25 @@
 package com.ealmeida.springbootionic.cursomc.resources;
 
+import com.ealmeida.springbootionic.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar() {
-        return "Rest funcionando";
+    public List<Categoria> listar() {
+
+        Categoria c1 = new Categoria(1, "Informática");
+        Categoria c2 = new Categoria(2, "Escritório");
+
+        List<Categoria> categorias = Arrays.asList(c1, c2);
+
+        return categorias;
     }
 }
