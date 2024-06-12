301a63337f25169625a045ce138605f9673aca28
Édipo Almeida <almeida.edipo@icloud.com>
2024-06-12 14:59:27 +0100

    init Categoria

--- cursomc/src/main/java/com/ealmeida/springbootionic/cursomc/CursomcApplication.java
+++ cursomc/src/main/java/com/ealmeida/springbootionic/cursomc/CursomcApplication.java
@@ -1,13 +1,30 @@
 package com.ealmeida.springbootionic.cursomc;
 
+import com.ealmeida.springbootionic.cursomc.domain.Categoria;
+import com.ealmeida.springbootionic.cursomc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria c1 = new Categoria(null, "Informática");
+		Categoria c2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(c1,c2));
+
+	}
 }
